8f9fdc7d2f9a4482f310a3b505d49a62de0fcd7d
ashutosh9897794237 <113291959+ashutosh9897794237@users.noreply.github.com>
2022-12-19 00:03:57 +0530

    number

--- solution.js
+++ solution.js
@@ -1,10 +1,10 @@
 function letterCombinations(input_digits) {
-    if(digits.length==0) return[];
+    if(input_digits.length==0) return[];
     const map = { '2' : 'abc', '3' : 'def', '4' : 'ghi',
                    '5' : 'jkl', '6' : ',mno', '7' : 'pqrs',
                    '8' : 'tuv', '9' : 'wxyz',};
            const queue=[''];
-           for(const digit of digits){
+           for(const digit of input_digits){
              const len = queue.length;
              for( let i=0; i<len; i++) {
               const current = queue.shift();
